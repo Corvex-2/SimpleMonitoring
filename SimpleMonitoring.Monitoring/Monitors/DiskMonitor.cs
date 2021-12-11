23b488635a51cc3590c0f5ee981f73c0449a073e
Funk <corvex166@gmail.com>
2021-12-11 16:09:04 +0100

    removed old unrequired files from .Monitoring

--- SimpleMonitoring.Monitoring/Monitors/DiskMonitor.cs
+++ SimpleMonitoring.Monitoring/Monitors/DiskMonitor.cs
@@ -1,10 +1,7 @@
 ﻿using SimpleMonitoring.Monitoring.System;
 using SimpleMonitoring.Utilites;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleMonitoring.Monitoring.Monitors
 {
--- SimpleMonitoring.Monitoring/Monitors/MemoryMonitor.cs
+++ SimpleMonitoring.Monitoring/Monitors/MemoryMonitor.cs
@@ -1,10 +1,8 @@
 ﻿using SimpleMonitoring.Monitoring.System;
 using SimpleMonitoring.Utilites;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleMonitoring.Monitoring.Monitors
 {
